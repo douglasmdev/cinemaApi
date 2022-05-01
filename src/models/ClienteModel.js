555f6c4610971a01c1b07f4b419a1c92b279a442
yonirg <yonirabinovicigherman@gmail.com>
2022-05-01 13:39:48 -0300

    Criação do constructor do model de clientes e exportação dele

---
+++ src/models/ClienteModel.js
@@ -0,0 +1,10 @@
+class ClienteModel {
+    constructor(cpf, nome, tel, email){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.tel = tel;
+        this.email = email;
+    }
+}
+
+export default ClienteModel
