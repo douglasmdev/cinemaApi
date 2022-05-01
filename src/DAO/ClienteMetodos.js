58aba9ecb87682e8494c5afff2868c167f70d934
yonirg <yonirabinovicigherman@gmail.com>
2022-05-01 16:33:11 -0300

    Criando o método listaClientes
    Método que lista todos os clientes

--- src/DAO/ClienteMetodos.js
+++ src/DAO/ClienteMetodos.js
@@ -1,1 +1,13 @@
 import Database from "../infra/Database";
+
+class ClienteMetodos{
+    static listaClientes() {
+        const query = 'SELECT * FROM clientes';
+        return new Promise((resolve, reject) =>{
+            Database.all(query, (error, registros) => {
+                if (error) reject(error.message);
+                else resolve({clientes: registros});    
+            });
+        });
+    }
+}
