41a0b170b43f00c7ff68a6cd8d8717c127e7127b
yonirg <yonirabinovicigherman@gmail.com>
2022-05-01 15:53:25 -0300

    Criando o READ(geral) do CRUD

--- src/controllers/ClienteController.js
+++ src/controllers/ClienteController.js
@@ -1,4 +1,17 @@
 import ClienteModel from "../models/ClienteModel";
 import ClienteMetodos from "../DAO/ClienteMetodos";
 
+class ClienteController{
+    static rotas(app) {
+        app.get('/clientes', async (req, res) => {
+            try {
+                const resposta = await ClienteMetodos.listaClientes();
+                res.status(200).json({mensagem: resposta});
+            } catch (error) {
+                res.status(400).json({erro: error.message});
+            }
+        });       
+    }
+}
+
 
