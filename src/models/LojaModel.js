85fb82e93d59fb6d02d1c3c044e983389ebf9228
DiegoTolotto8@gmail.com <diego.tolotto8@gmail.com>
2022-05-01 18:09:22 -0300

    feat: adding store model

---
+++ src/models/LojaModel.js
@@ -0,0 +1,10 @@
+class LojaModel {
+    constructor(id, idFilme, salas, assentos) {
+        this.id = id;
+        this.idFilme = idFilme;
+        this.salas = salas;
+        this.assentos = assentos;
+    }
+}
+
+export default LojaModel;
