57945fceeea9934571e28d1adb933e5e789937ee
Douglas Montes <douglasmontes.dev@gmail.com>
2022-05-03 15:40:49 -0300

    adicionado model do bilhete

---
+++ src/models/BilheteModel.js
@@ -0,0 +1,12 @@
+class BilheteModel {
+    
+    constructor(id, cpfCliente, idFilme, idLoja) {
+        this.id = id; 
+        this.cpfCliente = cpfCliente; 
+        this.idFilme = idFilme; 
+        this.idLoja = idLoja; 
+    }
+
+}
+
+export default BilheteModel;
